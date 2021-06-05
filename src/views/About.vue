ebf37505bf5d99be3b7dc1e469a5965c2dfdfd26
KOJI KUBOTA <kubotak@KOJInoMac-mini.local>
2021-06-06 08:17:43 +0900

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button class="btn" type="primary" @click="handleGoBackHome">GoBack Home Page</el-button>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "About",
+  setup() {
+    const router = useRouter();
+
+    const handleGoBackHome = () => {
+      router.push({ name: "Home" });
+    };
+
+    return { handleGoBackHome };
+  },
+});
+</script>
